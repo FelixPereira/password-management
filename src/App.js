deb69c057fc07ea6f2e720ab514071271ca460af
FelixPereira <felixpereira538@gmail.com>
2021-10-26 06:18:00 +0200

    Criação do arquivo db.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
 import React from 'react';
 import CardList from './Components/Cards/Card-list/Card-list';
+import passwords from './db'; 
 
 
 const App = () => {
-    const passwords = [
-        {
-            "id": 1,
-            "companyName": "devsoft", 
-            "username": "Félix",
-            "email": "felix.pereira@devsoft.co.ao", 
-            "password": "felix123",
-            "loginLink": "https://devsoft.co.ao/webmail"
-          },
-      
-          {
-            "id": 2,
-            "companyName": "devsoft", 
-            "username": "Félix",
-            "email": "felix.pereira@devsoft.co.ao", 
-            "password": "felix123",
-            "loginLink": "https://devsoft.co.ao/webmail"
-          },
-      
-          {
-            "id": 3,
-            "companyName": "devsoft", 
-            "username": "Félix",
-            "email": "felix.pereira@devsoft.co.ao", 
-            "password": "felix123",
-            "loginLink": "https://devsoft.co.ao/webmail"
-          },
-      
-          {
-            "id": 4,
-            "companyName": "devsoft", 
-            "username": "Félix",
-            "email": "felix.pereira@devsoft.co.ao", 
-            "password": "felix123",
-            "loginLink": "https://devsoft.co.ao/webmail"
-          }
-    ]
-
     return(
         <main className='container'>
             <CardList passwords={ passwords } />
---
+++ src/db.js
@@ -0,0 +1,39 @@
+const passwords = [
+  {
+    "id": 1,
+    "companyName": "devsoft", 
+    "username": "Félix",
+    "email": "felix.pereira@devsoft.co.ao", 
+    "password": "felix123",
+    "loginLink": "https://devsoft.co.ao/webmail"
+  },
+
+    {
+      "id": 2,
+      "companyName": "devsoft", 
+      "username": "Félix",
+      "email": "felix.pereira@devsoft.co.ao", 
+      "password": "felix123",
+      "loginLink": "https://devsoft.co.ao/webmail"
+    },
+
+    {
+      "id": 3,
+      "companyName": "devsoft", 
+      "username": "Félix",
+      "email": "felix.pereira@devsoft.co.ao", 
+      "password": "felix123",
+      "loginLink": "https://devsoft.co.ao/webmail"
+    },
+
+    {
+      "id": 4,
+      "companyName": "devsoft", 
+      "username": "Félix",
+      "email": "felix.pereira@devsoft.co.ao", 
+      "password": "felix123",
+      "loginLink": "https://devsoft.co.ao/webmail"
+    }
+]
+
+export default passwords;
