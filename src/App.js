816dbd6db28a082b339d4a00ee5666f683699d13
FelixPereira <felixpereira538@gmail.com>
2021-10-26 07:11:32 +0200

    Criação do componente Filter.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './app.css';
 import CardList from './Components/Cards/Card-list/Card-list';
+import Filter from './Components/Filter/Input';
 import passwords from './db'; 
 
 
 const App = () => {
     return(
         <main className='container'>
-            <CardList passwords={ passwords } />
+          <Filter />
+          <CardList passwords={ passwords } />
         </main>
     )
 }
