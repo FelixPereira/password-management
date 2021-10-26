8e25cb7f2c1717e650e3caa0aad95e35f615b374
FelixPereira <felixpereira538@gmail.com>
2021-10-26 06:54:43 +0200

    Refatorando o ficheiro db.js

--- src/db.js
+++ src/db.js
@@ -11,63 +11,63 @@ const passwords = [
   {
     "id": 2,
     "companyName": "devsoft", 
-    "username": "Félix",
-    "email": "felix.pereira@devsoft.co.ao", 
-    "password": "felix123",
+    "username": "Mauro",
+    "email": "mauro.culeca@devsoft.co.ao", 
+    "password": "mauro123",
     "loginLink": "https://devsoft.co.ao/webmail"
   },
 
   {
     "id": 3,
     "companyName": "devsoft", 
-    "username": "Félix",
-    "email": "felix.pereira@devsoft.co.ao", 
-    "password": "felix123",
+    "username": "Hermenegildo",
+    "email": "hermenegildo.saldanha@devsoft.co.ao", 
+    "password": "Hermenegildo123",
     "loginLink": "https://devsoft.co.ao/webmail"
   },
 
   {
     "id": 4,
     "companyName": "devsoft", 
-    "username": "Félix",
-    "email": "felix.pereira@devsoft.co.ao", 
-    "password": "felix123",
+    "username": "Beatriz",
+    "email": "beatriz.xavier@devsoft.co.ao", 
+    "password": "Beatriz123",
     "loginLink": "https://devsoft.co.ao/webmail"
   },
 
   {
-    "id": 4,
+    "id": 5,
     "companyName": "devsoft", 
-    "username": "Félix",
-    "email": "felix.pereira@devsoft.co.ao", 
-    "password": "felix123",
+    "username": "Valdemiro",
+    "email": "valdemiro.felgueiras@devsoft.co.ao", 
+    "password": "Valdemiro123",
     "loginLink": "https://devsoft.co.ao/webmail"
   },
 
   {
-    "id": 4,
+    "id": 6,
     "companyName": "devsoft", 
-    "username": "Félix",
-    "email": "felix.pereira@devsoft.co.ao", 
-    "password": "felix123",
+    "username": "Indira",
+    "email": "indira.tito@devsoft.co.ao", 
+    "password": "Indira123",
     "loginLink": "https://devsoft.co.ao/webmail"
   },
 
   {
-    "id": 4,
-    "companyName": "devsoft", 
-    "username": "Félix",
-    "email": "felix.pereira@devsoft.co.ao", 
-    "password": "felix123",
+    "id": 7,
+    "companyName": "Fulano", 
+    "username": "Fulano",
+    "email": "fulano.sicrano@devsoft.co.ao", 
+    "password": "Fulano123",
     "loginLink": "https://devsoft.co.ao/webmail"
   },
 
   {
-    "id": 4,
-    "companyName": "devsoft", 
-    "username": "Félix",
-    "email": "felix.pereira@devsoft.co.ao", 
-    "password": "felix123",
+    "id": 8,
+    "companyName": "Sicrano", 
+    "username": "Sicrano",
+    "email": "sicrano.fulano@devsoft.co.ao", 
+    "password": "Sicrano123",
     "loginLink": "https://devsoft.co.ao/webmail"
   }
 ]
