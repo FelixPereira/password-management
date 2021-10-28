163d8c356ffb9f89185e178e9dc42b20bcc00f13
FelixPereira <felixpereira538@gmail.com>
2021-10-28 07:04:17 +0200

    Criação do component Header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './app.css';
 import CardList from './Components/Cards/Card-list/Card-list';
 import Filter from './Components/Filter/Input';
 import passwords from './db'; 
+import Header from './Components/Header/Header';
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
 
   return(
       <main className='container'>
+        <Header />
         <Filter handleFilter={handleFilter} />
         <CardList passwords={ filteredPasswords } />
       </main>
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const header = () => {
+  return(
+    <header className='header'>
+      <div>
+        <img src='#' alt='Logotipo da devsoft'/>
+      </div>
+      <button>Adicionar</button>
+    </header>
+  )
+}
+
+export default header;
