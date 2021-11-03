c9d279aff6251b324e84b00236ad8ea6c1ee342f
FelixPereira <felixpereira538@gmail.com>
2021-11-03 13:53:57 +0200

    Adding style to the cards

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,10 +6,12 @@ const header = ({ handleAddCompany }) => {
 
   return(
     <header className='header'>
-      <div className='header_logo'>
-        <img src='https://devsoft.co.ao/wp-content/uploads/2020/06/cropped-devsoft.png' width='200px' alt='Logotipo da devsoft'/>
+      <div className='header__items'>
+        <div className='header_logo'>
+          <img src='https://devsoft.co.ao/wp-content/uploads/2020/06/cropped-devsoft.png' width='200px' alt='Logotipo da devsoft'/>
+        </div>
+        <button onClick={handleAddCompany} className='btn'>Adicionar</button>
       </div>
-      <button onClick={handleAddCompany} className='btn'>Adicionar</button>
     </header>
   )
 }
