3467962acc8a5829900c3ac525378924add5f2a8
FelixPereira <felixpereira538@gmail.com>
2021-11-12 21:56:31 +0200

    Adding link to logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ const header = ({ handleAddCompany }) => {
   return(
     <header className='header'>
       <div className='header__items'>
-        <Link to='/' className='header_logo'>
+        <Link to='/'>
           <img src='https://devsoft.co.ao/wp-content/uploads/2020/06/cropped-devsoft.png' width='200px' alt='Logotipo da devsoft'/>
         </Link>
         <button onClick={handleAddCompany} className='btn'>Adicionar</button>
